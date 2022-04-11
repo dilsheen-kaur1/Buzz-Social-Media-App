5cb2f4594f9705560ea3ce28125d4b7b4d0a877f
dilsheen-kaur1 <dilsheen.kaur@tothenew.com>
2022-04-11 12:27:47 +0530

    project framework

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <div>App</div>
+    </Router>
+    
+  );
+}
+
+export default App;
